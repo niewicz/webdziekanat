dd5cc0faa25be8136c9ae578f8197990472aebc4
tprochniewicz <tommpoland@gmail.com>
2016-11-04 10:07:21 -0700

    registration path disabled, devise layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,22 @@ class ApplicationController < ActionController::Base
  	protect_from_forgery with: :exception
  	before_action :authenticate_user!
  	rescue_from ActionController::RoutingError, with: :not_found
+ 	layout :layout_by_resource
 
 	def not_found
-  		redirect_to root_path
+		respond_to do |format|
+      		format.html { redirect_to root_path, notice: "Error, wrong params in the request - record could not be found" }
+      		format.json { head :not_found }
+      	end
 	end
+
+  	protected
+
+  	def layout_by_resource
+    	if devise_controller?
+      		"devise"
+    	else
+      		"application"
+    	end
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root 'home#index'
   
-  devise_for :users
+  devise_for :users, path_names: {
+  	sign_up: ''
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
