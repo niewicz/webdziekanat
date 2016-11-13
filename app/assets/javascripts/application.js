ca5f40ff19c6eea31de1e30d48e5058b8b969af2
tprochniewicz <tommpoland@gmail.com>
2016-11-13 07:08:11 -0800

    grades view --> needs adjusting for rwd compact

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,27 +37,57 @@ $(document).on('turbolinks:load', function(){
     });
 
     $('#sem1').click(function(){
-        $('#sem-selected').html("Semester 1 - Winter 2014");
+        $('#sem-selected-container').show();
+        $('#sem-dropdown').html('Semester 1 - Winter 2014 <i class="fa fa-angle-down"></i>');
         $("#sem-list").slideToggle("fast");
+        $('#id-sem1').show();
+        $('#id-sem2').hide();
+        $('#id-sem3').hide();
+        $('#id-sem4').hide();
+        $('#id-sem5').hide();
     });
 
     $('#sem2').click(function(){
-        $('#sem-selected').html("Semester 2 - Spring 2015");
+        $('#sem-selected-container').show();
+        $('#sem-dropdown').html('Semester 2 - Spring 2015 <i class="fa fa-angle-down"></i>');
         $("#sem-list").slideToggle("fast");
+        $('#id-sem1').hide();
+        $('#id-sem2').show();
+        $('#id-sem3').hide();
+        $('#id-sem4').hide();
+        $('#id-sem5').hide();
     });
 
     $('#sem3').click(function(){
-        $('#sem-selected').html("Semester 3 - Winter 2015");
+        $('#sem-selected-container').show();
+        $('#sem-dropdown').html('Semester 3 - Winter 2015 <i class="fa fa-angle-down"></i>');
         $("#sem-list").slideToggle("fast");
+        $('#id-sem1').hide();
+        $('#id-sem2').hide();
+        $('#id-sem3').show();
+        $('#id-sem4').hide();
+        $('#id-sem5').hide();
     });
 
     $('#sem4').click(function(){
-        $('#sem-selected').html("Semester 4 - Spring 2016");
+        $('#sem-selected-container').show();
+        $('#sem-dropdown').html('Semester 4 - Spring 2016 <i class="fa fa-angle-down"></i>');
         $("#sem-list").slideToggle("fast");
+        $('#id-sem1').hide();
+        $('#id-sem2').hide();
+        $('#id-sem3').hide();
+        $('#id-sem4').show();
+        $('#id-sem5').hide();
     });
 
     $('#sem5').click(function(){
-        $('#sem-selected').html("Semester 5 - Winter 2016");
+        $('#sem-selected-container').show();
+        $('#sem-dropdown').html('Semester 5 - Winter 2016 <i class="fa fa-angle-down"></i>');
         $("#sem-list").slideToggle("fast");
+        $('#id-sem1').hide();
+        $('#id-sem2').hide();
+        $('#id-sem3').hide();
+        $('#id-sem4').hide();
+        $('#id-sem5').show();
     });
 });	
